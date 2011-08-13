3a5e154e682e6f2c02b00699562ea40c94b89da8
Brian Chang <bchanged@gmail.com>
2011-08-12 22:44:33 -0700

    suite for running tests in the p4blame module

---
+++ p4blame/src/test/java/com/github/bchang/p4/blame/P4BlameSuite.java
@@ -0,0 +1,17 @@
+package com.github.bchang.p4.blame;
+
+import gw.test.Suite;
+import junit.framework.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: bchang
+ * Date: 8/12/11
+ * Time: 10:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class P4BlameSuite extends Suite {
+  public static Test suite() {
+    return new P4BlameSuite().withPackages("com.github.bchang.p4.blame");
+  }
+}
